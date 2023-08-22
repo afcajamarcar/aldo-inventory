ad7e92b74102f9f0928c9ba6a548e952000e1580
Andres <afcajamarcar@unal.edu.co>
2023-08-22 15:53:34 -0400

    add model validations to Notifications and tests for the controller

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,6 @@
 class Notification < ApplicationRecord
+    validates :store, presence: true
+    validates :model, presence: true
+    validates :inventory, presence: true
+    validates :notification_status, presence: true
 end
--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should store a new Notification" do
+    post "/notifications",
+      params: {
+        notification: {
+          store: "my store",
+          model: "a shoe model",
+          inventory: "0",
+          notification_status: "No Stock",
+        },
+      }
+    assert_response :success
+  end
+
+  test "should throw an exception if one param is missing when saving a new Notification" do
+    post "/notifications",
+      params: {
+        notification: {
+          store: "my store",
+          model: "a shoe model",
+          inventory: "0",
+        },
+      }
+    assert_response :error
+  end
 end
