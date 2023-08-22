1abe6639b264e2bae3afdf9239b11adfdf31c7bd
Andres <afcajamarcar@unal.edu.co>
2023-08-22 14:51:38 -0400

    update notifications controller to actually store a record, improve identation

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,21 @@
 class NotificationsController < ApplicationController
-    before_action :sanitize_data, only: [:create]
+  before_action :sanitize_data, only: [:create]
 
-    def create
-        @notification = Notification.new(notification_params)
-    end
+  def create
+    @notification = Notification.new(notification_params)
 
-    def notification_params
-        params.require(:notification).permit(:store, :model, :inventory, :notification_status)
+    if @notification.save
+      render json: { message: @notification.created_at }
+    else
+      render json: { errors: @notification.errors }, status: :internal_server_error
     end
+  end
 
-    def sanitize_data
-        params[:inventory] = params[:inventory].to_i
-    end
+  def notification_params
+    params.require(:notification).permit(:store, :model, :inventory, :notification_status)
+  end
+
+  def sanitize_data
+    params[:inventory] = params[:inventory].to_i
+  end
 end
